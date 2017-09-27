80ed9fb515cf9315586ebbb23026c988eaf3fc56
Michael Staehling <Michael.staehling@gmail.com>
2017-09-27 13:18:34 -0400

    add component header list of languages

--- app/index.js
+++ app/index.js
@@ -1,20 +1,12 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
+var PropTypes = require('prop-types');
 require('./index.css');
+var App = require('./components/App');
 
 //state
 //lifecycle event
 //UI...only one that's required
-
-class App extends React.Component {
-  render() {
-    return (
-      <div>
-      Hello World!
-      </div>
-    )
-  }
-}
 
 ReactDOM.render(
   <App />,
